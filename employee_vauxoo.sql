84e7d2b81a511b602e0a2c920c83a629cc27986c
caarlosdamian <dapecarlos@gmail.com>
2017-07-25 21:08:47 -0500

    Empleados,Departamentos
    Se agrego tabla empleados y departamentos, se agrego informacion en los campos.

--- employee_vauxoo.sql
+++ employee_vauxoo.sql
@@ -4,12 +4,33 @@
 --       Consider add ';' at end sentence.
 
 CREATE TABLE employee (
+    id seria PRIMARY KEY,
+    department_id integer REFERENCES employee_department,
+    first_name varchar(50) NOT NULL,
+    last_name varchar(50) NOT NULL
+    
+    
+    
 );
 
 CREATE TABLE employee_department (
+    id serial PRIMARY KEY,
+    name varchar(50),
+    description varchar(50)
 );
 
 CREATE TABLE employee_hobby (
 );
 
+INSERT INTO employee_department (name, description) VALUES ('Cocteleria', 'departamento de Cocteleria');
+INSERT INTO employee_department (name, description) VALUES ('juegos','departamento de juegos');
+INSERT INTO employee_department (name, description) VALUES ('maquillaje', 'departamento de maquillaje');
+INSERT INTO employee_department (name, description) VALUES ('mascotas', 'departamento de mascotas');
+INSERT INTO employee_department (name, description) VALUES ('computacion', 'departamento de computacion');
+INSERT INTO employee_department (name, description) VALUES ('mercadotecnia', 'departamento de mercadotecnia');
+
+INSERT INTO employee (first_name, last_name,department_id) VALUES ('Carlos','Damian',5);
+INSERT INTO employee (first_name, last_name,department_id) VALUES ('Uriel','Gonzalez',4);
+INSERT INTO employee (first_name, last_name,department_id) VALUES ('Diana','chavez',1);
+INSERT INTO employee (first_name, last_name,department_id) VALUES ('elena','Chavez',6);
 -- ...
