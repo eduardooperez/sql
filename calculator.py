26dd478663bf2df192a39209648d9c210437dab7
caarlosdamian <dapecarlos@gmail.com>
2017-07-25 21:27:18 -0500

    calculadora
    calculadora

---
+++ calculator.py
@@ -0,0 +1,24 @@
+
+"""
+Your module documentation here
+"""
+
+
+class CalculatorClass(object):
+    """
+    Your class documentation here
+    """
+
+    def sum(self, num_list):
+        """
+        Metodo que recibe de entrada una lista de numeros
+        y regresa la suma de estos
+        """
+        # variable donde se guardara la suma de todos los numeros
+        resultado = 0
+        # ciclo donde se suman los numeros de la lista
+        for num in num_list:
+            # suma del valor a la variable resultado
+            resultado = resultado + num
+        # retorno del resultado de la suma de la lista de numeros
+        return resultado
