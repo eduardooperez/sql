8717c0b2e3c2f8ab11e6386ffbe5ca397151dfa7
caarlosdamian <dapecarlos@gmail.com>
2017-07-25 21:19:57 -0500

    Se agrego tabla hobbies
    hobby de empleado

--- employee_vauxoo.sql
+++ employee_vauxoo.sql
@@ -20,7 +20,18 @@ CREATE TABLE employee_department (
 );
 
 CREATE TABLE employee_hobby (
+     id serial PRIMARY KEY,
+    name varchar(50),
+    description varchar(50)
+    
 );
+
+CREATE TABLE employee_hobbies (
+	employee_id integer REFERENCES employee,
+	hobby_id integer REFERENCES employee_hobby,
+	PRIMARY KEY (employee_id, hobby_id)
+);
+
 
 INSERT INTO employee_department (name, description) VALUES ('Cocteleria', 'departamento de Cocteleria');
 INSERT INTO employee_department (name, description) VALUES ('juegos','departamento de juegos');
@@ -33,4 +44,18 @@ INSERT INTO employee (first_name, last_name,department_id) VALUES ('Carlos','Dam
 INSERT INTO employee (first_name, last_name,department_id) VALUES ('Uriel','Gonzalez',4);
 INSERT INTO employee (first_name, last_name,department_id) VALUES ('Diana','chavez',1);
 INSERT INTO employee (first_name, last_name,department_id) VALUES ('elena','Chavez',6);
+INSERT INTO employee (first_name, last_name,department_id) VALUES ('Diana','chavez',1);
+
+INSERT INTO employee_hobby (name, description) VALUES ('lectura', 'leer libros');
+INSERT INTO employee_hobby (name, description) VALUES ('videojuegos', 'jugar videojuegos');
+INSERT INTO employee_hobby (name, description) VALUES ('peliculas', 'ver peliculas');
+
+INSERT INTO employee_hobbies (employee_id, hobby_id) VALUES (1, 1);
+INSERT INTO employee_hobbies (employee_id, hobby_id) VALUES (1, 2);
+INSERT INTO employee_hobbies (employee_id, hobby_id) VALUES (2, 3);
+INSERT INTO employee_hobbies (employee_id, hobby_id) VALUES (2, 1);
+INSERT INTO employee_hobbies (employee_id, hobby_id) VALUES (3, 2);
+INSERT INTO employee_hobbies (employee_id, hobby_id) VALUES (3, 3);
+INSERT INTO employee_hobbies (employee_id, hobby_id) VALUES (4, 1);
+INSERT INTO employee_hobbies (employee_id, hobby_id) VALUES (4, 3);
 -- ...
